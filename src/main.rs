ae3b49eafc2e321e78c71510f0e50577968ee425
Thomas <denhollander.thomas@gmail.com>
2018-06-26 21:45:14 +0200

    New main

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,17 @@ use config::ConfigFile;
 use failure::Error;
 
 fn main() -> Result<(), Error> {
-    let config = ConfigFile::from_file("worlds/spheres_medium.json")?;
+    let config = ConfigFile::from_file("worlds/plates_medium.json")?;
     let force = config.to_world()?.force_on(0);
     println!("Calculated force: ({}, {}, {})", force.x, force.y, force.z);
 
     let force = config.to_world()?.force_on(1);
     println!("Calculated force: ({}, {}, {})", force.x, force.y, force.z);
 
-    let config = ConfigFile::from_file("worlds/plates_medium.json")?;
-    let force = config.to_world()?.force_on(0);
-    println!("Calculated force: ({}, {}, {})", force.x, force.y, force.z);
+
+    let config = ConfigFile::from_file("worlds/spheres_medium.json")?;
+    // let force = config.to_world()?.force_on(0);
+    // println!("Calculated force: ({}, {}, {})", force.x, force.y, force.z);
 
     let force = config.to_world()?.force_on(1);
     println!("Calculated force: ({}, {}, {})", force.x, force.y, force.z);
